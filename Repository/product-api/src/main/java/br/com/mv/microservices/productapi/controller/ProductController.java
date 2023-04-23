6b747d203d02bf89f1443fb5b58bc876a290026e
Mateus Valentim de Oliveira <mateus.valentim@ibm.com>
2023-04-22 21:17:53 -0300

    Add Dockerfile

--- Repository/product-api/src/main/java/br/com/mv/microservices/productapi/controller/ProductController.java
+++ Repository/product-api/src/main/java/br/com/mv/microservices/productapi/controller/ProductController.java
@@ -1,11 +1,14 @@
 package br.com.mv.microservices.productapi.controller;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +16,9 @@ import java.util.Map;
 @RequestMapping("/api")
 public class ProductController {
 
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(
+            DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
     @GetMapping("status")
     public ResponseEntity<Map <String, Object>> getStatusApi(){
 
@@ -21,5 +27,6 @@ public class ProductController {
         response.put("status", "UP");
         response.put("httpStatus", HttpStatus.OK.value());
         return ResponseEntity.ok(response);
+
     }
 }
